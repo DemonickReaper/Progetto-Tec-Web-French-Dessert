6b1723269d25739ade798bcf8d7f42d929b6b356
Stefano <liverani.stefano1@gmail.com>
2018-01-26 13:24:27 +0100

    store

--- visual.php
+++ visual.php
@@ -88,6 +88,7 @@
 		<script src="js/ann/annotator.unsupported.min.js"></script>
 		<script src="js/Chart.js"></script>
 		<script src="js/ann/coffeescript.js"></script>
+		<script src="js/ann/store.js"></script>
 		<script type="text/coffeescript">
 window.onerror = (msg, file, line) ->
 
