4dd4cde15764e175582aad147522103476ed7f65
DemonickReaper <mattexd@libero.it>
2018-02-03 18:29:49 +0100

    Fixato annotato per utenti che non sono loggati (leggi la descrizione)
    se un utente non é loggato ora puó leggere tutte le annotaizoni pubbliche,
    quando prova a crearne una riceve un errore contenente il consiglio ad effettuare il login.

--- server/annotations/create.php
+++ server/annotations/create.php
@@ -1,60 +1,56 @@
 <?php
 
 $object = file_get_contents('php://input');
-
-
-//$object = $_POST['data'];
-//$object = '{"permissions":{"read":[],"update":[],"delete":[],"admin":[]},"user":"fff","ranges":[{"start":"/div[1]/p[1]/small[1]","startOffset":1,"end":"/div[1]/p[1]","endOffset":99}],"quote":"rench pronunciation: ​[makaʁɔ̃]) is a sweet meringue-based","text":"sadfsadf","uri":2005216}';
 $rows = json_decode($object,true);
-
-session_start();
 
 
 if (isset($_SESSION['datauser']) && $_SESSION['datauser'] == true) {
-    $username = $_SESSION['datauser'];
-    class MyDB extends SQLite3
+  session_start();
+  $username = $_SESSION['datauser'];
+
+  class MyDB extends SQLite3
+  {
+    function __construct()
     {
-      function __construct()
-      {
-        $this->open('../progetto.db');
-      }
+      $this->open('../progetto.db');
     }
-    $db = new MyDB();
-    if (!$db) {
-      echo $db->lastErrorMsg();
-    } else {
+  }
+  $db = new MyDB();
+  if (!$db) {
+    echo $db->lastErrorMsg();
+  } else {
       //echo "Opened database successfully\n";
-    }
-    $read = $rows['permissions']['read'];
-    $update = $rows['permissions']['update'];
-    $delete = $rows['permissions']['delete'];
-    $admin = $rows['permissions']['admin'];
-    $read = implode(',',$read);
-    if($read === ''){
-      $read = 'NULL';
-    }
-    $update = implode(',',$update);
-    $delete = implode(',',$delete);
-    $admin = implode(',',$admin);
-    $pageid = $rows['uri'];
-    $start = $rows['ranges'][0]['start'];
-    $startOffset = $rows['ranges'][0]['startOffset'];
-    $end = $rows['ranges'][0]['end'];
-    $endOffset = $rows['ranges'][0]['endOffset'];
-    $text = $rows['text'];
-    $user = $rows['user'];
-    $sql = "INSERT INTO ANNOTATIONS (pageid,startt,endd,startOffset,endOffset,textt,user,readd,updatee,deletee,adminn) VALUES (".$pageid.",'".$start."','".$end."',".$startOffset.",".$endOffset.",'".$text."','".$user."','".$read."','".$update."','".$delete."','".$admin."');";
-    echo $sql;
-    $db->query($sql);
+  }
+  $read = $rows['permissions']['read'];
+  $update = $rows['permissions']['update'];
+  $delete = $rows['permissions']['delete'];
+  $admin = $rows['permissions']['admin'];
+  $read = implode(',', $read);
+  if ($read === '') {
+    $read = 'NULL';
+  }
+  $update = implode(',', $update);
+  $delete = implode(',', $delete);
+  $admin = implode(',', $admin);
+  $pageid = $rows['uri'];
+  $start = $rows['ranges'][0]['start'];
+  $startOffset = $rows['ranges'][0]['startOffset'];
+  $end = $rows['ranges'][0]['end'];
+  $endOffset = $rows['ranges'][0]['endOffset'];
+  $text = $rows['text'];
+  $user = $rows['user'];
+  $sql = "INSERT INTO ANNOTATIONS (pageid,startt,endd,startOffset,endOffset,textt,user,readd,updatee,deletee,adminn) VALUES (" . $pageid . ",'" . $start . "','" . $end . "'," . $startOffset . "," . $endOffset . ",'" . $text . "','" . $user . "','" . $read . "','" . $update . "','" . $delete . "','" . $admin . "');";
+  echo $sql;
+  $db->query($sql);
 
-    $sql = "SELECT MAX(id) FROM ANNOTATIONS";
-    $res = $db->query($sql);
-    $resu = $res->fetchArray(SQLITE3_ASSOC);
-    $id = $resu['MAX(id)'];
+  $sql = "SELECT MAX(id) FROM ANNOTATIONS";
+  $res = $db->query($sql);
+  $resu = $res->fetchArray(SQLITE3_ASSOC);
+  $id = $resu['MAX(id)'];
 
-    $db->close();
+  $db->close();
 
-    header("HTTP/1.1 303 See Other");
-    header("Location: http://localhost/Progetto-qualcosa-che-si-mangia/server/annotations/read.php?id=".$id);
+  header("HTTP/1.1 303 See Other");
+  header("Location: http://localhost/Progetto-qualcosa-che-si-mangia/server/annotations/read.php?id=" . $id);
 }
 ?>
--- server/annotations/index.php
+++ server/annotations/index.php
@@ -1,8 +1,15 @@
 <?php 
-session_start();
-$pageid = $_GET["uri"];
+
 if (isset($_SESSION['datauser']) && $_SESSION['datauser'] == true) {
+  session_start(); 
   $username = $_SESSION['datauser'];
+}
+else {
+  $username = 'guest';
+}
+
+  $pageid = $_GET["uri"];
+
   class MyDB extends SQLite3
   {
     function __construct()
@@ -17,7 +24,7 @@ if (isset($_SESSION['datauser']) && $_SESSION['datauser'] == true) {
     //echo "Opened database successfully\n";
   }
 
-  $sql1 = "SELECT * FROM ANNOTATIONS WHERE user = '". $username ."' OR readd = 'NULL' AND pageid ='" . $pageid . "';";
+  $sql1 = "SELECT * FROM ANNOTATIONS WHERE user = '" . $username . "' OR readd = 'NULL' AND pageid ='" . $pageid . "';";
 
   $res = $db->query($sql1);
 
@@ -35,17 +42,17 @@ if (isset($_SESSION['datauser']) && $_SESSION['datauser'] == true) {
     $total = $total->fetchArray(SQLITE3_ASSOC);
     $rows['total'] = $total['COUNT(*)'];
     $i = 0;
-    
+
     while ($row = $res->fetchArray(SQLITE3_ASSOC)) {
-      
+
       $ranges[0]['start'] = $row['startt'];
       $ranges[0]['end'] = $row['endd'];
       $ranges[0]['startOffset'] = $row['startOffset'];
       $ranges[0]['endOffset'] = $row['endOffset'];
       $rows['rows'][$i]['text'] = $row['textt'];
       $rows['rows'][$i]['id'] = $row['id'];
-      $rows['rows'][$i]['user'] = $row['user'];   
-      $rows['rows'][$i]['ranges'] =  $ranges;
+      $rows['rows'][$i]['user'] = $row['user'];
+      $rows['rows'][$i]['ranges'] = $ranges;
       $rows['rows'][$i]['uri'] = $row['pageid'];
       $read = $row['readd'];
       $read = explode(',', $read);
@@ -59,13 +66,13 @@ if (isset($_SESSION['datauser']) && $_SESSION['datauser'] == true) {
       $rows['rows'][$i]['permissions']['update'] = $update;
       $rows['rows'][$i]['permissions']['delete'] = $delete;
       $rows['rows'][$i]['permissions']['admin'] = $admin;
-      $i++;                                                                      
-    } 
+      $i++;
+    }
     $jsonObject = json_encode($rows);
     header('Content-Type: application/json');
     echo $jsonObject;
   }
   $db->close();
-}
+
 ?>
 
